41d7f706899cc4e413595ccfa0bf02ac0a117393
Ruslan Abramov <r.o.abramov@gmail.com>
2018-09-24 11:18:34 +0300

    make use of custom font

--- App.js
+++ App.js
@@ -5,16 +5,36 @@ import { theme } from './src/styles/theme';
 import Header from './src/components/Header/Header';
 import { Layout } from './src/components/Layout/Layout';
 import CalendarFeed from './src/screen/CalendarFeed/CalendarFeed';
+import Expo from 'expo';
+import { loadAsync } from 'expo/src/Font';
 
-export default () => (
-  <ThemeProvider theme={theme}>
-    <Layout.Wrapper>
-      <Layout.Header>
-        <Header title="Scheduler" />
-      </Layout.Header>
-      <Layout.Content>
-        <CalendarFeed />
-      </Layout.Content>
-    </Layout.Wrapper>
-  </ThemeProvider>
-);
+export default class extends React.Component {
+  state = {
+    assetsLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Expo.Font.loadAsync({
+      Rubik: require('./src/assets/Rubik-Regular.ttf'),
+    });
+    this.setState({ assetsLoaded: true });
+  }
+
+  render() {
+    if (!this.state.assetsLoaded) {
+      return null;
+    }
+    return (
+      <ThemeProvider theme={theme}>
+        <Layout.Wrapper>
+          <Layout.Header>
+            <Header title="Webpurple's Scheduler" />
+          </Layout.Header>
+          <Layout.Content>
+            <CalendarFeed />
+          </Layout.Content>
+        </Layout.Wrapper>
+      </ThemeProvider>
+    );
+  }
+}
--- src/components/Header/atoms.js
+++ src/components/Header/atoms.js
@@ -1,6 +1,6 @@
 // @flow
 import styled from 'styled-components';
-import { getColor } from '../../styles/theme';
+import { getColor, getFontFamily } from '../../styles/theme';
 
 export const Container = styled.View`
   flex-direction: row;
@@ -14,8 +14,9 @@ export const Container = styled.View`
 
 export const Title = styled.Text`
   color: ${getColor('lipstick')};
-  font-size: 22px;
+  font-family: Rubik;
   font-weight: bold;
+  font-size: 22px;
 `;
 
 export const Logo = styled.Image`
